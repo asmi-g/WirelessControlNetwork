2df35014a3c1f059c17ca1766b86762f3b1b5a4a
asmi-g <asmi.gujral@gmail.com>
2024-03-24 04:34:49 -0400

    ESP32 Controller Code Complete

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,37 @@
 #define WS_PORT 443
 #define WS_URL ""
 
+#define MSG_SIZE 256
+
 WiFiMulti wifiMulti;
 WebSocketsClient wsClient;
+
+void sendErrorMessage(const char *error){ 
+  //writes error message to client
+  char msg[MSG_SIZE];
+
+  sprintf(msg, "{\"action\":\"msg\",\"type\":\"error\",\"body\":\"%s\"}", error);
+
+  wsClient.sendTXT(msg);
+}
+
+void validInputMessage(){
+  //does the opposite of sendErrorMessage, it validates that user's input was accepted
+    wsClient.sendTXT("{\"action\":\"msg\",\"type\":\"status\",\"body\":\"success\"}");
+}
+
+uint8_t toMode(const char *val){
+  //compares value to three of the given pin modes, return value if any are true
+  if(strcmp(val, "output") == 0) {
+    return OUTPUT;
+  }
+
+  if (strcmp(val, "input_pullup") == 0){
+    return INPUT_PULLUP;
+  }
+
+  return INPUT;
+}
 
 void handleMessage(uint8_t * payload)
 {
@@ -24,12 +53,59 @@ void handleMessage(uint8_t * payload)
   if (error) {
     Serial.print(F("deserializeJson() failed: "));
     Serial.println(error.f_str());
+    sendErrorMessage(error.c_str());
     return;
   }
+
+  // Perform validation on JSON document
+  if(!doc["type"].is<const char *>()){
+    sendErrorMessage("Invalid message type format");
+    return;
+  }
+
+  // Perform validation on JSON document
+  if(strcmp(doc["type"], "cmd") == 0){ //use strcmp, or string compare
+    // Test if body is valid
+    if(!doc["body"].is<JsonObject>()){
+      sendErrorMessage("Invalid command body");
+      return;
+    }
+
+    if(strcmp(doc["body"]["type"], "pinMode") == 0){ //string compare
+      // Here, the toMode function just performs validation for the pinMode function
+      pinMode(doc["body"]["pin"], toMode(doc["body"]["mode"]));
+      validInputMessage();
+      return;
+    }
+
+    if(strcmp(doc["body"]["type"], "digitalWrite") == 0){ //string compare
+      // Handles digitalWrite command
+      digitalWrite(doc["body"]["pin"], doc["body"]["value"]);
+      validInputMessage();
+      return;
+    }
+
+    if(strcmp(doc["body"]["type"], "digitalRead") == 0){ //string compare
+      // Handles digitalRead command, sends value that was read to client
+      auto value = digitalRead(doc["body"]["pin"]);
+      char msg[MSG_SIZE];
+      sprintf(msg, "{\"action\":\"msg\",\"type\":\"output\",\"body\":%d}", value);
+      wsClient.sendTXT(msg);
+      return;
+    }
+
+    // Handles errors associated with nested if statement for command type
+    sendErrorMessage("Unsupported command type");
+    return;
+  }
+
+  // Handles errors associated with handleMessage function (i.e, no action was done to the LED pin due to the command not being in the codebase)
+  sendErrorMessage("Unsupported message type");
+  return;
 }
 
 void onWSEvent(WStype_t type, uint8_t * payload, size_t length){
-  //handles events w/ switch case statement
+  //handles websockets events w/ switch case statement, depending on status of socket
   switch(type){
     case WStype_CONNECTED:
       Serial.println("WS Connected");
@@ -63,6 +139,8 @@ void setup() {
 }
 
 void loop() {
+
+  //pulls led pin to HIGH to turn it on if connected to wifi
   digitalWrite(LED_BUILTIN, WiFi.status() == WL_CONNECTED);
   wsClient.loop();
 }
